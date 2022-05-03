5b43fe55fdd3a98191e05dafe8905680d29f869b
Umut Bilal Okur <umutokur11@outlook.com>
2022-05-03 21:05:39 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,59 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Car car1 = new Car { Id = 6, BrandId = 6, ColorId = 3, DailyPrice = 134000,Description = "Scoda SuperB",ModelYear = 2017};
+            Car car2 = new Car { Id = 7, BrandId = 3, ColorId = 1, DailyPrice = 120000, Description = "Ford Ranger", ModelYear = 2016 };
+
+            PrintAll(carManager);
+            Console.WriteLine("----------------------------------");
+
+            carManager.Add(car1);
+            carManager.Add(car2);
+            PrintAll(carManager);
+
+            Console.WriteLine("----------------------------------");
+            carManager.Delete(car2);
+            PrintAll(carManager);
+
+            Console.WriteLine("----------------------------------");
+            carManager.Update(new Car { Id = 1, BrandId = 6, ColorId = 4, DailyPrice = 105000,Description = "Scoda Octavia", ModelYear = 2020});
+            PrintAll(carManager);
+
+            Console.WriteLine("----------------------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description + ": " + car.DailyPrice );
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            foreach (var car in carManager.GetByBrandId(6))
+            {
+                Console.WriteLine(car.Description);
+            }
+
+
+
+
+        }
+
+        static void PrintAll(CarManager carManager)
+        {
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id = 1, BrandId = 1 , ColorId = 1 , DailyPrice = 200000, Description = "BMW 320", ModelYear = 2018 },
+                new Car { Id = 2, BrandId = 1 , ColorId = 2 , DailyPrice = 300000, Description = "BMW 520", ModelYear = 2021 },
+                new Car { Id = 3, BrandId = 2 , ColorId = 2 , DailyPrice = 150000, Description = "Ford Focus", ModelYear = 2018 },
+                new Car { Id = 4, BrandId = 3 , ColorId = 1 , DailyPrice = 250000, Description = "Wolkswagen Passat", ModelYear = 2017 },
+                new Car { Id = 5, BrandId = 4 , ColorId = 3 , DailyPrice = 500000, Description = "Porche", ModelYear = 2019 },
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByBrandId(int brandId)
+        {
+            return _cars.Where(c=>c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c=> c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        public int Size()
+        {
+            return _cars.Count;
+        }
+    }
+}
